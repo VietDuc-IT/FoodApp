b46b11ad628bf227b116c0726c7a483e4793677f
VietDuc-IT <levietduc4566@gmail.com>
2023-05-26 22:01:55 +0700

    Update

--- app/src/main/java/com/levietduc/foodapp/activity/MainActivity.java
+++ app/src/main/java/com/levietduc/foodapp/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.levietduc.foodapp;
+package com.levietduc.foodapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.levietduc.foodapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
