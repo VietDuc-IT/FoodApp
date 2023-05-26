5c48488c3730205ae463bf59687d29321b091ccd
VietDuc-IT <levietduc4566@gmail.com>
2023-05-26 23:02:15 +0700

    Update

--- app/src/main/java/com/levietduc/foodapp/activity/IntroActivity.java
+++ app/src/main/java/com/levietduc/foodapp/activity/IntroActivity.java
@@ -2,16 +2,29 @@ package com.levietduc.foodapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.levietduc.foodapp.R;
+import com.levietduc.foodapp.databinding.ActivityIntroBinding;
 
 public class IntroActivity extends AppCompatActivity {
+
+    ActivityIntroBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
-        //text
+        //setContentView(R.layout.activity_intro);
+        binding = ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this,MainActivity.class));
+            }
+        });
     }
 }
