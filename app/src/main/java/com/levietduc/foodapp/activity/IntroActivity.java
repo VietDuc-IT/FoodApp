77453f1168eee0b9920a1ebcfaedc63a602e18c9
VietDuc-IT <levietduc4566@gmail.com>
2023-05-26 22:09:23 +0700

    Update

---
+++ app/src/main/java/com/levietduc/foodapp/activity/IntroActivity.java
@@ -0,0 +1,17 @@
+package com.levietduc.foodapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.levietduc.foodapp.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        //text
+    }
+}
